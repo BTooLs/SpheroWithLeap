6186f5d8aa4b693ed75d1aa052cec1f7fce54faf
B.G.Adrian <btools@gmail.com>
2014-07-25 12:02:13 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 
 service bluetooth start
-sdptool browse #this fixes a bug in ubuntu/bluetooth/blueman locked port
+#sdptool browse
 
-#TODO get address from hcitool scan | 
+CHANNEL=1 
+SPHERO=`hcitool scan | grep Sphero | head -1`
+MAC=`echo "$SPHERO" | awk '{print $1}'`
+NAME=`echo "$SPHERO" | awk '{print $2}'`
+DEV="/dev/rfcomm$CHANNEL"
+if [[ -z $SPHERO ]]; then
+	echo "No sphero found"
+	exit	
+fi
+
+echo "Found sphero $NAME with $MAC "
+
 #rfcomm release 0 68:86:E7:03:C2:8A
-rfcomm release 1 68:86:E7:03:C2:8A
+rfcomm release $CHANNEL $MAC
 
 echo "Mounting sphere"
-rfcomm bind 1 68:86:E7:03:C2:8A
-rfcomm connect 1 68:86:E7:03:C2:8A 1
+rfcomm bind $CHANNEL $MAC
+rfcomm connect $CHANNEL $MAC
 rfcomm -a
 sleep 1
 echo "Starting app"
-node --debug app.js
-sleep 1
-echo "Unmount sphere"
-rfcomm release 1 68:86:E7:03:C2:8A
-rfcomm -a
+node --debug app.js $DEV
+#sleep 1
+#echo "Unmount sphere"
+#rfcomm release $CHANNEL $MAC
+#rfcomm -a
